49a51a697e405ac51360a3b86986b38530dab5e3
owaisyosuf <157351741+owaisyosuf@users.noreply.github.com>
2024-04-12 15:11:42 +0500

    cli number guessing game

---
+++ CLI.ts
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const randomNo = Math.floor(Math.random() * 6 + 1);
+
+console.log("please select a no between 1 to 6");
+
+const ans = await inquirer.prompt([
+  { message: "select your number", type: "number", name: "userGuessNumber" },
+]);
+
+if (ans.userGuessNumber===randomNo){
+  console.log("congratulation you win the game")
+}else{
+  console.log("you lose the game")
+};
